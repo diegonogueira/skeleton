080929cca0f7d1e7bf0cdccc0c3ae4204207d2d8
Diego Nogueira <diegoalvareznogueira@gmail.com>
2020-01-27 17:47:11 -0300

    tmp

--- lib/resolver/resolver.ex
+++ lib/resolver/resolver.ex
@@ -10,6 +10,7 @@ defmodule Skeleton.Resolver do
     output: nil,
     halted: false,
     error: nil,
+    request_datetime: nil,
     permission: %{
       ids: [],
       checks: []
@@ -26,8 +27,9 @@ defmodule Skeleton.Resolver do
           action: action,
           args: args,
           context: info.context,
+          source: info.source,
           definition: info.definition,
-          resource: nil,
+          request_datetime: NaiveDateTime.utc_now,
           permission: %{
             ids: [],
             checks: []
@@ -56,25 +58,6 @@ defmodule Skeleton.Resolver do
   def ensure_not_authenticated(%{halted: true} = resolver), do: resolver
   def ensure_not_authenticated(resolver), do: resolver.helper._ensure_not_authenticated(resolver)
 
-  # Put source and resource
-
-  def put_source(resolver, func \\ nil)
-  def put_source(%{halted: true} = resolver, _), do: resolver
-  def put_source(resolver, nil), do: put_source(resolver, &resolver.module._source/1)
-  def put_source(resolver, func), do: put_source_or_resource(resolver, :source, func.(resolver))
-
-  def put_resource(resolver, func \\ nil)
-  def put_resource(%{halted: true} = resolver, _), do: resolver
-  def put_resource(resolver, nil), do: put_resource(resolver, &resolver.module._resource/1)
-  def put_resource(resolver, func), do: put_source_or_resource(resolver, :resource, func.(resolver))
-
-  def put_source_or_resource(resolver, name, nil) do
-    put_error(resolver, "#{to_string(name)} not found")
-  end
-  def put_source_or_resource(resolver, name, data) do
-    Map.put(resolver, name, data)
-  end
-
   # Resolve query
 
   def resolve_query(%{halted: true} = resolver, _), do: {:error, resolver.error}
@@ -91,40 +74,22 @@ defmodule Skeleton.Resolver do
 
   def resolve_mutation(%{halted: true} = resolver, _), do: {:error, resolver.error}
   def resolve_mutation(resolver, callback) do
-    {:ok, resource} =
-      resolver
-      |> put_output(callback.(resolver))
-      |> resolver.helper._after_resolve_mutation()
-
-    key = get_module_name(resource)
-
-    {:ok, %{key => resource}}
-  end
-
-  defp get_module_name(struct) do
-    struct.__struct__
-    |> Macro.underscore()
-    |> String.split("/")
-    |> List.last()
-    |> String.to_atom()
+    resolver
+    |> put_output(callback.(resolver))
+    |> resolver.helper._after_resolve_mutation()
   end
 
   # Permission
 
-  def check_source_permission(%{halted: true} = resolver, _), do: resolver
-  def check_source_permission(resolver, check) do
-    ensure_permission(resolver, resolver.module._source_permission(resolver), check)
+  def check_permission(%{halted: true} = resolver, _, _), do: resolver
+  def check_permission(resolver, permission, check) do
+    ensure_permission(resolver, permission, check)
   end
 
-  def check_resource_permission(%{halted: true} = resolver, _), do: resolver
-  def check_resource_permission(resolver, check) do
-    ensure_permission(resolver, resolver.module._resource_permission(resolver), check)
+  defp ensure_permission(resolver, permission, check) do
+    ensure_permission(resolver, permission, check, nil)
   end
-
-  defp ensure_permission(resolver, permission_module, check) do
-    ensure_permission(resolver, permission_module, check, nil)
-  end
-  defp ensure_permission(resolver, permission_module, check, id) do
+  defp ensure_permission(resolver, permission, check, id) do
     data = resolver.resource || resolver.source || %{}
 
     # TODO: Tratar para esses casos
@@ -141,10 +106,10 @@ defmodule Skeleton.Resolver do
 
     preloaded =
       resolver
-      |> permission_module.preload()
+      |> permission.preload()
       |> List.first()
 
-    if permission_module.check(check, preloaded, resolver) do
+    if permission.check(check, preloaded, resolver) do
       resolver
     else
       put_error(resolver, "not authorized")
